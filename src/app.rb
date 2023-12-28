882dcc218d5d15a281e25c690f13e799de96beb4
lukblan <luk.blanko@gmail.com>
2023-12-28 11:03:17 -0300

    Change dependencies

--- src/app.rb
+++ src/app.rb
@@ -1,8 +1,8 @@
 require_relative "./ui/console_interface"
-require_relative './domain/board_factory'
+require_relative './domain/board/board_factory'
 
 # Domain
-board_factory = BoardFactory.new("-", "x")
+board_factory = BoardFactory.new("_", "x", "F")
 board = board_factory.create_board(10, 13)
 
 # UI
